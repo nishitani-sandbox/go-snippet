014be0754570e52b0fe5117ecc19841a3ef31c67
nishitaniyuki <yuki.nishitani85@gmail.com>
2018-01-21 19:45:02 +0900

    signal

---
+++ signal/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	exitChan := make(chan int)
+	go func() {
+		for {
+			s := <-signalChan
+			switch s {
+			case syscall.SIGHUP:
+				fmt.Println("hungup")
+			case syscall.SIGINT:
+				fmt.Println("warikomi")
+			case syscall.SIGTERM:
+				fmt.Println("force stop")
+				exitChan <- 0
+			case syscall.SIGQUIT:
+				fmt.Println("stop and core dump")
+				exitChan <- 0
+			default:
+				fmt.Println("Unknown signal.")
+				exitChan <- 0
+			}
+		}
+	}()
+
+	code := <-exitChan
+	os.Exit(code)
+}
