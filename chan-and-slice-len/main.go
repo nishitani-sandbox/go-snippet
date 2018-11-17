74ccb00f57d2928c8ddcaf1c94059c80fb579a44
nishitaniyuki <yuki.nishitani85@gmail.com>
2018-11-17 16:15:12 +0900

    add chan-and-slice-len result

--- chan-and-slice-len/main.go
+++ chan-and-slice-len/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	s1 := make([]int, 50)
-	s2 := make(chan int, 50)
+	fmt.Println(len(s1)) // 50
 
-	fmt.Println(len(s1))
-	fmt.Println(len(s2))
+	s2 := make(chan int, 50)
+	fmt.Println(len(s2)) // 0
+	s2 <- 0
+	fmt.Println(len(s2)) // 1
 }
