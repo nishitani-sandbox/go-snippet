72900e3f1367098d18050cec470cf3fbec128e74
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2019-11-26 07:01:32 +0000

    add pid-taskstats

---
+++ pid-taskstats/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/mdlayher/taskstats"
+)
+
+func main() {
+	c, err := taskstats.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) == 1 {
+		log.Fatal("please specify PID")
+	}
+	pid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	stats, err := c.PID(pid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", stats)
+}
