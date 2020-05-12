42e854b3185b81b8032366ed6deabdc8e2a46e08
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2020-05-12 22:58:04 +0000

    add json-map-decode

---
+++ json-map-decode/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Endpoint struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
+}
+
+func main() {
+	f, err := os.Open("./sample.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var m map[string]*Endpoint
+	err = json.NewDecoder(f).Decode(&m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for k, v := range m {
+		fmt.Printf("key: %s\n", k)
+		fmt.Printf("host: %s\n", v.Host)
+		fmt.Printf("port: %d\n", v.Port)
+	}
+}
