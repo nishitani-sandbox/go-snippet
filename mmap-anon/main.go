e856d6dbe8ff0b46eadbadce33b28578f3d556f8
nishitaniyuki <yuki.nishitani85@gmail.com>
2020-05-27 09:00:51 +0000

    update mmap-anon

--- mmap-anon/main.go
+++ mmap-anon/main.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
-	mem, err := syscall.Mmap(-1, 0, 1<<20, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_ANON|syscall.MAP_PRIVATE)
+	mem, err := syscall.Mmap(-1, 0, 1<<30, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_ANON|syscall.MAP_PRIVATE)
 	if err != nil {
 		log.Fatal(err)
 	}
-	syscall.Munmap(mem)
+	defer syscall.Munmap(mem)
+
+	for i := 0; i < len(mem); i++ {
+		mem[i] = 'a'
+	}
 }
